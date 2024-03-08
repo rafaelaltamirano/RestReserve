eaa6281ce4a5adaed3ae6f4bd6363bd904f4f2bb
rafaelaltamirano <rafaelaltamirano_@outlook.com>
2024-03-08 14:05:57 +0100

    add BuildSrc module and base-module.gradle for dependencies

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+import org.gradle.kotlin.dsl.`kotlin-dsl`
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+}
+
+
---
+++ buildSrc/src/main/java/Android.kt
@@ -0,0 +1,4 @@
+object Android {
+    private const val desugaringVersion = "2.0.4"
+    const val desugaring = "com.android.tools:desugar_jdk_libs:$desugaringVersion"
+}
